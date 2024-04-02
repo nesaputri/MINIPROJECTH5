a8f632e6da72a61a1754dcbbadb322ca8784638e
ecaaapd <89679089+nesaputri@users.noreply.github.com>
2024-04-02 08:21:26 +0700

    second commit

--- question2.js
+++ question2.js
@@ -1,25 +1,52 @@
 const generateWinner = () => {
-  const matchList = [["mu", "city"], ["munchen", "mu"], ["city", "munchen"]];
-  const scores = {
-    mu: 0,
-    city: 0,
-    munchen: 0,
-  };
+
+  const matchList = [
+    ["mu", "city"],
+    ["munchen", "mu"],
+    ["city", "munchen"]
+  ]
+
+  const resultMatch = [0, 1, 0]
+  let muPoints = 0;
+  let cityPoints = 0;
+  let munchenPoints = 0;
+
 
   for (let i = 0; i < matchList.length; i++) {
-    const [home, away] = matchList[i];
-    if (home > away) scores[home] += 3;
-    else if (home < away) scores[away] += 3;
-    else {
-      scores[home] += 1;
-      scores[away] += 1;
+    if (matchList[i][1] === 'mu') {
+      if (resultMatch[i] === 1) {
+        muPoints += 3;
+      }
+      else {
+        cityPoints += 3;
+      }
+    }
+    if (matchList[i][0] === 'munchen') {
+      if (resultMatch[i] === 1) {
+        muPoints += 3;
+      }
+      else {
+        munchenPoints += 3;
+      }
+    }
+    if (matchList[i][1] === 'munchen') {
+      if (resultMatch[i] === 1) {
+        munchenPoints += 3;
+      }
+      else {
+        cityPoints += 3;
+      }
     }
   }
 
-  const results = Object.values(scores);
-  const winningScore = Math.max(...results);
-  return Object.keys(scores).filter(team => scores[team] === winningScore)[0];
+  console.log(`${ muPoints } ${ munchenPoints } ${ cityPoints }`)
+
+  if (munchenPoints > Math.max(muPoints, cityPoints)) {
+    return 'munchen';
+  }
+  else return Math.max(muPoints, cityPoints) === muPoints ? 'mu' : 'city';
+
 }
 
 const winner = generateWinner();
-console.log(winner);
+console.log(winner)
